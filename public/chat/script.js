1a1672f029f29096fa11db1ca571e40b2affb790
saurabh <saurabh7481@gmail.com>
2022-04-09 18:49:16 +0530

    added real time chat fetching

--- public/chat/script.js
+++ public/chat/script.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", () => {
-    getChats();
+    setInterval(() => {
+        getChats()
+    }, 1000);
 
     const sendBtn = document.querySelector("#send");
     sendBtn.addEventListener("click", sendMessage);
@@ -27,7 +29,9 @@ async function getChats(){
     try{
         const res = await axios.get("http://localhost:3000/api/chat/get");
         if(res.status === 200){
-            console.log(res.data);
+            renderChats(res.data.chats);
+        } else {
+            alert("Something went wrong")
         }
     } catch(err){
         if(err.response.status === 401){
@@ -36,4 +40,31 @@ async function getChats(){
             alert("Cannot get chats");
         }
     }
+}
+
+function renderChats(chats) {
+    const chatBox = document.querySelector(".chat-box");
+    chats.forEach(chat => {
+        let d = new Date(chat.createdAt);
+        let time = ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2);
+        let tmp = "";
+        if(parseInt((chat.id)%2) !== 0){
+            tmp = `
+            <div class="message primary">
+                ${chat.message}
+                <div class="timestamp">${time}</div>
+            </div>
+            `
+        } else {
+            tmp = `
+            <div class="message secondary">
+                ${chat.message}
+                <div class="timestamp">${time}</div>
+            </div>
+            `
+        }
+
+        chatBox.innerHTML += tmp;
+        
+    })
 }
